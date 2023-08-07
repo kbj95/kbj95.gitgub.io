2820f9b5d7912eb96f18e49c966436105a38365a
kbj95 <gwonbongjeong@gmail.com>
2023-08-07 18:13:21 +0900

    new

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// loading
+// (function(){
+//     window.onload = function(){
+//     const preloader = document.querySelector('.page-loading');
+//     const wrap = document.getElementById('test');
+//     test.style.display = "none";
+//     preloader.classList.remove('active');
+//     setTimeout(function(){
+//         preloader.remove();
+//         test.style.display = "block";
+//     },1000);
+//     };
+// })();
